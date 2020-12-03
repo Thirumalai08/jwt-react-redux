e7bace7ffb0f5d5934b178651bff27f49236c422
Thirumalai08 <thirunambi422@gmail.com>
2020-12-03 13:15:48 +0530

    Basic Login & Logout

--- src/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux'
-import signinReducer from './userReducer'
-
-export default combineReducers({
-    signIn: signinReducer
-})
--- src/store.js
+++ src/store.js
@@ -20,12 +20,3 @@ const store = createStore(
     composeWithDevTools(applyMiddleware(thunk))
 )
 export default store
-/*import {createStore,applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-
-import rootReducer from './reducer'
-
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)))
-
-export default store*/
